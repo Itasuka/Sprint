939e6e316a39b48b21b958122647e7737f4ca75c
Noémie <nonoemielv.15@gmail.com>
2021-12-11 13:24:09 +0100

    pas mal de truc

--- modele/modele.php
+++ modele/modele.php
@@ -8,3 +8,39 @@ function getConnect(){
     return $connexion;
 }
 
+//on recherche les clients ayant tel nom et telle date de naissance, retourne un tableau des id des ces clients
+function chercherUnIdClient($nomclient,$datenaissanceclient){
+    $connexion=getConnect();
+    $requete="select idcli from client where nomcli='$nomclient' and datenaissancecli='$datenaissance'";
+    $resultat=$connexion->query($requete);
+    $resultat->setFetchMode(PDO::FETCH_OBJ);
+    $tableaudesclients=$resultat->fetchall();
+    $resultat->closeCursor();
+    return $tableaudesclients;
+}
+
+function syntheseClient($id){
+    $connexion=getConnect();
+    $requete="select *,*,*,nomemploye from client natural join contrat natural join compte natural join employe"
+    $resultat=$connexion->query($requete);
+    $resultat->setFetchMode(PDO::FETCH_OBJ);
+    $tableauduclient=$resultat->fetchall();
+    $resultat->closeCursor();
+    return $tableauduclient;
+}
+
+function creerEmploye($id,$mdp,$nom,$prenom,$categorie){
+    $connexion=getConnect();
+    $requete="insert into employe values ($id, '$mdp', '$nom', '$prenom', '$categorie')" ; 
+    $resultat=$connexion->query($requete);
+    $resultat->closeCursor();
+}
+
+//bien chercher l'employer à modifier avant d'utiliser cette méthode pour avoir la valeur $id
+function modifierEmploye($id,$newid,$mdp){
+    $connexion=getConnect();
+    $requete="modify into employe values ($newid, '$mdp', '$nom', '$prenom', '$categorie') where id='$id'" ;
+    $resultat=$connexion->query($requete);
+    $resultat->closeCursor();
+}
+

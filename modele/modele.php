ba401ffad20a346d161cd2367643a3b4b771eec0
Nicolas <schivre.nicolas@gmail.com>
2021-12-11 11:41:04 +0100

    getconnect

--- modele/modele.php
+++ modele/modele.php
@@ -1,1 +1,10 @@
 <?php
+require_once('connect.php');
+
+function getConnect(){
+    $connexion=new PDO('mysql:host='.SERVEUR.';dbname='.BDD,USER,PASSWORD);
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connexion->query('SET NAMES UTF8');
+    return $connexion;
+}
+

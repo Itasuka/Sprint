5b32a95347d521e637ba665d0c20ff21447774f7
benjamin.roy <benjamin.roy00@orange.fr>
2021-12-12 22:11:44 +0100

    suite de la journée

---
+++ vue/gabarit_directeur.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+ 		<head>
+   			 <meta charset="utf-8">
+   			 <link rel="stylesheet" href="style/style.css">
+        </head>
+        <body>
+        <header>
+        </header>
+                <form>
+                        <fieldset>
+                                <legend></legend>
+                                        <p>
+                                                <input type="submit" value="accès des employer" name="ModifLogEmployer" />
+                                                <input type="submit" value="créé/modif/supp un contrat" name="C/M/S_Contrat" />
+                                                <input type="submit" value="la liste du pièce à fournir" name="C/M/S_Piece" />
+                                                <input type="submit" value="Statistique" name="Stat" />
+                                        </p>
+                        </fieldset>
+                </form>
+        </body>

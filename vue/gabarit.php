1119ce6698fa75e866af43b6c8389bc4fd6fa2ae
benjamin.roy <benjamin.roy00@orange.fr>
2021-12-13 12:19:56 +0100

    premier commit de gabarit de la journée

--- vue/gabarit.php
+++ vue/gabarit.php
@@ -7,25 +7,11 @@
    			 <script type="text/javascript" src="script/script.js"></script>
         </head>
         <body>
-        	<form>
-        		<fieldset>
-        			<legend> Veillez saisir votre login et votre mots de passe</legend>
-        			<p>
-						<label for="pseudo">Votre login:</label>
-						<input type="text" name="login" id="login" placeholder="saisir votre login" required  />
-					</p>
-					<p>
-						<label for="pass">Votre mot de passe :</label>
-						<input type="password" name="mdp" id="mdp" placeholder="saisir votre mot de passe" required />
-					</p>
-					<p>
-						<input type="submit" name="okay" value="envoyer">
-					</p>
-        		</fieldset>
-        		
-        	</form>
-
-
+        	<header>
+        	</header>
+        	<?php echo $contenueCat
+        	echo $contenue
+        	?>
         </body>
 
 </html>
